f3cc91565c83762b8172d4b96ce9d5af4ddaaf66
Christopher Enam Franklin <cef1911@hotmail.com>
2023-12-23 23:46:22 -0500

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import altair as alt
+import pandas as pd
+from PIL import Image
+
+
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:75px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+#st.markdown('<p class="big-font">Merry Christmas To Stacey Franklin!!</p>', unsafe_allow_html=True)
+st.markdown('<p class="big-font">Merry Christmas To Stacey Franklin!!</p>', unsafe_allow_html=True)
+
+image = Image.open("twistedpizzasf.PNG")
+
+st.image(image, caption='Stacey Franklin, Merry Christmas 2023')
+
+st.write("Tribute To Stacey Franklin")
+
+
+# imagetwo = Image.open("birthday.jpeg")
+
+# st.image(imagetwo, caption='Amanda Franklin')
+
+
+imagethree = Image.open("memories.png")
+
+st.image(imagethree, caption='Stacey Franklin')
+
+
+st.write("Love You Stacey, Chris")
+
+st.balloons()
+
+st.snow()
+
